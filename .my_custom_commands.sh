2f830f63634bfdbaf3163a3903d7114abad3b375
lleonard188 <lleonard188@gmail.com>
2018-12-13 11:35:35 +0800

    Intial commit

---
+++ .my_custom_commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bashi
+function cromwell() {
+	java -jar 'C:\\Program Files\cromwell\cromwell-36.jar' run $1
+}
+function womtool() {
+ 	 java -jar 'C:\\Program Files\cromwell\womtool-36.jar' $1
+}		
+function work() {
+	cd /mnt/c/users/len/dropbox/programming/methylationpipeline
+}
