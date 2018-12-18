584b6074d979156646af0bd158bf30f3c7e8a708
lleonard188 <lleonard188@gmail.com>
2018-12-18 13:46:17 +0800

    Fixed womtool command

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -3,7 +3,7 @@ function cromwell() {
 	java -jar 'C:\\Program Files\cromwell\cromwell-36.jar' run $1
 }
 function womtool() {
- 	 java -jar 'C:\\Program Files\cromwell\womtool-36.jar' $1
+ 	 java -jar 'C:\\Program Files\cromwell\womtool-36.jar'  $1 $2
 }		
 function work() {
 	cd /mnt/c/users/len/dropbox/programming/methylationpipeline
