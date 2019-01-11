be9bf310445f8d0617963a3136eebd992fa3a49d
lleonard188 <lleonard@gmail.com>
2019-01-11 10:41:37 +0800

    Fixed Cromwell command

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function cromwell() {
-	java -jar /usr/bin/cromwell-36.jar
+	java -jar /usr/bin/cromwell-36.jar $1 $2
 }
 function womtool() {
  	 java -jar 'C:\\Program Files\cromwell\womtool-36.jar'  $1 $2
