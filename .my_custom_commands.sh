48233978ddc6bf504910b00f0ce9c6c142d9ce52
lleonard188 <lleonard@gmail.com>
2019-01-11 10:24:28 +0800

    Fixed path for Linux

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -6,5 +6,5 @@ function womtool() {
  	 java -jar 'C:\\Program Files\cromwell\womtool-36.jar'  $1 $2
 }		
 function work() {
-	cd /mnt/c/users/len/dropbox/programming/methylationpipeline
+	cd /home/len/Dropbox/Programming/MethylationPipeline
 }
